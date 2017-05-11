bfec35f7202a579f3b2593f127f5815d3d266fc3
clar17 <clarenciotan@gmail.com>
2017-05-11 16:48:06 +0800

    commit gulpfile before adding uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,34 @@
 var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 var tsify = require("tsify");
-// var ts = require("gulp-typescript");
-// var tsProject = ts.createProject("tsconfig.json");
-
-// gulp.task("default", function () {
-//     return tsProject.src()
-//         .pipe(tsProject())
-//         .js.pipe(gulp.dest("dist"));
-// });
+var gutil = require('gulp-util');
 
 var paths = {
     pages: ['src/*.html']
 };
 
-gulp.task("copy-html", function () {
+var watchedBrowserify = watchify(browserify({
+    basedir: ".",
+    debug: true,
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+}).plugin(tsify));
+
+gulp.task("copy-html",  () => {
     return gulp.src(paths.pages)
         .pipe(gulp.dest("dist"));
 });
 
-gulp.task("default", ["copy-html"], function(){
-    return browserify({
-        basedir: '.',
-        debug: true,
-        entries: ['src/main.ts'],
-        cache: {},
-        packageCache:{}
-    })
-    .plugin(tsify)
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest("dist"));
-});
+var bundle = () => {
+    return watchedBrowserify
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest("dist"));
+};
+
+gulp.task("default", ["copy-html"], bundle);
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", gutil.log);
